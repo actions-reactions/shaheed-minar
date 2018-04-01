45975e84ab1475d6e4669054be9fda375c2e8072
Prot Toy <31077074+ProttoyX@users.noreply.github.com>
2018-04-01 12:29:29 +0600

    :100:

--- main.c
+++ main.c
@@ -536,7 +536,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(600, 600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Line");
+    glutCreateWindow("Shaheed Minar");
     init();
     glutDisplayFunc(display);
     glutMainLoop();
